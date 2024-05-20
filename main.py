0d36cf58247e0fe16553fd8efe4a4e89fb405cdb
alireza <rly01309@gmail.com>
2024-05-20 20:19:52 +0330

    add project & show project

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import re
 import hashlib
 from rich.console import Console
 from libs.log import log
-from office.views import UserViewSet, ProjectViewSet
+from office.views import UserViewSet, ProjectViewSet, UserProjectViewSet
 from office.models import ProjectModel
 
 
-def show_users():
+def show_users(user_id: str):
     view = UserViewSet()
     pprint(view.list())
 
@@ -55,7 +55,7 @@ def check_activation(username: str, view: UserViewSet) -> bool:
     return list(filter(lambda item: item["username"] == username, view.list()["objects"]))[0].get("is_active")
 
 
-def sign_up():
+def sign_up(user_id: str):
     view = UserViewSet()
     user_info = dict()
     username = console.input("[bold cyan]Enter your username : [/bold cyan]")
@@ -81,7 +81,7 @@ def sign_up():
     # show_menu("main",)
 
 
-def log_in():
+def log_in(user_id: str):
     view = UserViewSet()
     username = console.input("[bold cyan]Enter your username : [/bold cyan]")
     if username == "return":
@@ -107,7 +107,7 @@ def log_in():
         show_menu("logged", list(filter(lambda item: item["username"] == username, view.list()["objects"]))[0].get("id"))
 
 
-def users_activity():
+def users_activity(user_id: str):
     view = UserViewSet()
     users = list(map(lambda item: item["username"], view.list()["objects"]))
     user_activity = dict()
@@ -116,33 +116,136 @@ def users_activity():
     print(user_activity)
     user_input = console.input("Enter username to change activity: ")
 
-    
-def show_projects():
-    view = ProjectViewSet()
-    pprint(view.list())
 
-
-def add_project():
+def project_environment(project_id: str):
+    pview = ProjectViewSet()
+    upview = UserProjectViewSet()
     view = UserViewSet()
-    username = console.input("Enter your username: ")
-    if users := view.filter(username=username):
-        user_id = users[0].get("id")
-        title = console.input("Enter your title: ")
-        project = ProjectModel(title=title, leader_id=user_id)
-        project.save()
-        members_count = int(console.input("Enter your count of members: "))
-        for index in range(1, members_count + 1):
-            member_username = console.input(f"{index} - Enter the username of member: ")
-            if m_users := view.filter(username=member_username):
-                member_id = m_users[0].get("id")
-                project.add_member(member_id=member_id)
-            else:
-                console.print("this username does not exist")
-    else:
-        print("the input is not valid.")
+    console.print("Title : ", list(filter(lambda item: item["id"] == project_id, pview.list()["objects"]))[0].get("title"))
+    leader_id = list(filter(lambda item: item["id"] == project_id, pview.list()["objects"]))[0].get("leader_id")
+    console.print("Leader : ", list(filter(lambda item: item["id"] == leader_id, view.list()["objects"]))[0].get("username"))
+    users = list(filter(lambda item: item["project_id"] == project_id, upview.list()["objects"]))
+    console.print("Users : ")
+    for user in users:
+        if user.get("user_id") != leader_id:
+            console.print(list(filter(lambda item: item["id"] == user.get("user_id"), view.list()["objects"]))[0].get("username"))
 
 
-def show_menu(type: str, user_id: int):
+def add_task():
+    pass
+
+
+def show_tasks():
+    pass
+
+
+def manage_task():
+    pass
+
+
+def delete_user():
+    pass
+
+
+def delete_project():
+    pass
+
+
+def show_project_options(user_id: str, project_id: str):
+    options = {
+        "Add task": add_task,
+        "Show tasks": show_tasks,
+        "Manage task": manage_task,
+        "Delete users": delete_user,
+        "Delete project": delete_project,
+    }
+    options_list = list(options.keys())
+    for index in range(len(options_list)):
+        print(f"{index + 1}. {options_list[index]}")
+    user_input = int(input("Enter your Option: ")) - 1
+    if user_input == -1:
+        return
+    func = options[options_list[user_input]]
+    # func(user_id)
+    # show_project_options(type, user_id)
+
+
+def show_projects(user_id: str):
+    view = UserProjectViewSet()
+    pview = ProjectViewSet()
+    user_projects_id = list()
+    console.print("You are leader of these projects :")
+    for project in list(filter(lambda item: item["leader_id"] == user_id, pview.list()["objects"])):
+        user_projects_id.append(project.get("id"))
+        console.print(f"{len(user_projects_id)}. {project.get("title")}")
+    print("\nYou are normal user in these projects :")
+    projects = list(filter(lambda item: item["user_id"] == user_id, view.list()["objects"]))
+    for project in projects:
+        is_ok = True
+        for lproject in list(filter(lambda item: item["leader_id"] == user_id, pview.list()["objects"])):
+            if project.get("project_id") == lproject.get("id"):
+                is_ok = False
+        if is_ok:
+            user_projects_id.append(project.get("project_id"))
+            console.print(f"{len(user_projects_id)}. {list(filter(lambda item: item["id"] == project.get("project_id"), pview.list()["objects"]))[0].get("title")}")
+    user_number = int(console.input("Enter number of project to open:"))
+    if user_number == 0:
+        return
+    project_environment(user_projects_id[user_number-1])
+    show_project_options(user_id, user_projects_id[user_number-1])
+
+
+def can_add(project_id: str, user_id: str, count: int) -> bool:
+    view = UserViewSet()
+    pview = UserProjectViewSet()
+    if username_exist(list(filter(lambda item: item["id"] == user_id, view.list()["objects"]))[0].get("username"), view):
+        users_in_project = list(filter(lambda item: item["project_id"] == project_id, pview.list()["objects"]))
+        for i in range(count):
+            if users_in_project[i].get("user_id") == user_id:
+                console.print()
+                return False
+        return True
+    return False
+
+
+def title_exist(title: str,) -> bool:
+    pview = ProjectViewSet()
+    if len(list(filter(lambda item: item["title"] == title, pview.list()["objects"]))) == 0:
+        return True
+    return False
+
+
+def add_project(user_id: str):
+    view = UserViewSet()
+    title = console.input("[bold cyan]Enter your title: [/bold cyan]")
+    if title == "return":
+        return
+    while not title_exist(title):
+        if title == "return":
+            return
+        title = console.input("[bold red]There is a project with this title.[/bold red]\n"
+                              "[bold yellow]Please try again : [/bold yellow]")
+    project = ProjectModel(title=title, leader_id=user_id)
+    project.save()
+    project.add_member(member_id=user_id)
+    members_count = int(console.input("Enter your count of members: "))
+    index = 1
+    while index < members_count + 1:
+        member_username = console.input(f"{index} - Enter the username of member: ")
+        if member_username == "return":
+            return
+        if not username_exist(member_username, view):
+            console.print("There is no user with this username.")
+            continue
+        member_id = list(filter(lambda item: item["username"] == member_username, view.list()["objects"]))[0].get("id")
+        if can_add(project.id, list(filter(lambda item: item["username"] == member_username, view.list()["objects"]))[0].get("id"), index):
+            project.add_member(member_id=member_id)
+            index = index + 1
+        else:
+            console.print("You cant add this user.")
+
+
+def show_menu(type: str, user_id: str):
     view = UserViewSet()
     options = {
         "Show users": show_users,
@@ -155,6 +258,8 @@ def show_menu(type: str, user_id: int):
     options_list = list(options.keys())
     if type == "main":
         options_list.pop(0)
+        options_list.pop(2)
+        options_list.pop(2)
         options_list.pop(2)
     elif not list(filter(lambda item: item["id"] == user_id, view.list()["objects"]))[0].get("is_admin"):
         for i in range(4):
@@ -169,7 +274,7 @@ def show_menu(type: str, user_id: int):
     if user_input == -1:
         return
     func = options[options_list[user_input]]
-    func()
+    func(user_id)
     show_menu(type, user_id)
 
 
